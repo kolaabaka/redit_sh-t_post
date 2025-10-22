2a0bbfad98efd5fe12478d4f46bb36e393caab68
KolaBaka <kolamba040301@gmail.com>
2025-10-22 20:19:59 +0300

    fix commit, move files

--- model/message.go
+++
@@ -1,7 +0,0 @@
-package model
-
-type Message struct {
-	Name    string
-	Message string
-	Date    string
-}
--- model/message_wall_template.go
+++
@@ -1,6 +0,0 @@
-package model
-
-type MessageWallTempalte struct {
-	Header      string
-	MessageList []Message
-}
